6eace5cb6b856a15bf18d5475f365a9d9b733d1f
monicaiyb <monicaiyb@gmail.com>
2023-03-13 22:54:12 +0300

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StaffApplication.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        public string myFirstTextLiteral;
+        public string Name => (string)TempData[nameof(Name)];
+        public void OnGet()
+        {
+            this.myFirstTextLiteral = "Hello, world!";
+        }
+        public IActionResult OnPost([FromForm] string name)
+        {
+            TempData["Name"] = name;
+            return RedirectToPage("Index");
+        }
+
+    }
+}
