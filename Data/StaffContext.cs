7389d928190d9dbb14f285887751c31ff8a9cb6f
monicaiyb <monicaiyb@gmail.com>
2023-03-21 22:46:15 +0300

    Edit dbContext folder

---
+++ Data/StaffContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StaffApplication.Models;
+
+namespace StaffApplication.Data
+{
+    public class StaffContext : DbContext
+    {
+        public StaffContext(DbContextOptions<StaffContext> options) : base(options)
+        {
+        }
+
+
+
+        public DbSet<Staff> Staff { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+    }
+}
