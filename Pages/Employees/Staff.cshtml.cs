695a80744d8b18532aa432b199c3f76c319efd92
monicaiyb <monicaiyb@gmail.com>
2023-03-21 23:03:39 +0300

    Add Staff UI

---
+++ Pages/Employees/Staff.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StaffApplication.Pages.Employees
+{
+    public class StaffModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
